c2b38a313dba34d142bc958d0b260e08de484170
mateoguaman <mateo.guaman1998@gmail.com>
2018-07-12 16:55:43 -0400

    Made the read/write a bit more robust

--- LSM9DS1test.ino
+++ LSM9DS1test.ino
@@ -8,9 +8,18 @@
 
 #define dt 0.02 //Loop time = 20 ms
 byte buff[6];
+/*
 int accRaw[3];
 int gyrRaw[3];
 int magRaw[3];
+*/
+/* IMU Data */
+//Maybe this works better?
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+double magX, magY, magZ;
+int16_t tempRaw;
+
 float rate_gyr_x = 0.0; //[deg/s]
 float rate_gyr_y = 0.0; //[deg/s]
 float rate_gyr_z = 0.0; //[deg/s] 
@@ -31,16 +40,16 @@ void setup() {
 
   //Enable Gyro
   writeTo(GYR_ADDRESS, CTRL_REG4, 0b00111000); //z, y, x axes enabled
-  writeTo(GYR_ADDRESS, CTRL_REG1_G, 0b10111000); //Gyro ODR = 476Hz, 2000 dps
+  writeTo(GYR_ADDRESS, CTRL_REG1_G, 0b10111000); //Gyro ODR = 476Hz, 2000 dps, check rate that works best
   writeTo(GYR_ADDRESS, ORIENT_CFG_G, 0b00111000); //Swap orientation, needed?
 
   //Enable Accelerometer
   writeTo(ACC_ADDRESS, CTRL_REG5_XL, 0b00111000); //z, y, x axes enabled
-  writeTo(ACC_ADDRESS, CTRL_REG6_XL, 0b00101000); // +/- 16g
+  writeTo(ACC_ADDRESS, CTRL_REG6_XL, 0b00101000); // +/- 16g, could less range make it better?
 
   //Enable the magnetometer
   writeTo(MAG_ADDRESS, CTRL_REG1_M, 0b10011100) // Temp compensation enabled,Low power mode mode,80Hz ODR
-  writeTo(MAG_ADDRESS, CTRL_REG2_M, 0b01000000);   // +/-12gauss
+  writeTo(MAG_ADDRESS, CTRL_REG2_M, 0b01000000);   // +/-12gauss, need this?
   writeTo(MAG_ADDRESS, CTRL_REG3_M, 0b00000000);   // continuos update
   writeTo(MAG_ADDRESS, CTRL_REG4_M, 0b00000000);   // lower power mode for Z axis
   
@@ -53,20 +62,30 @@ void loop() {
 
 }
 
-void writeTo(int device, byte address, byte data) {
+uint8_t writeTo(int device, byte address, byte data) {
   Wire.beginTransmission(device);
   Wire.write(address);
   Wire.write(data);
-  Wire.endTransmission();
+  uint_8 return_code = Wire.endTransmission();
+  if (return_code) {
+    Serial.print(F("writeTo failed: "));
+    Serial.println(return_code);
+  }
+  return return_code;
 }
 
-void readFrom(int device, byte address, int num_bytes, byte buff[]) {
+uint_8 readFrom(int device, byte address, int num_bytes, byte buff[]) {
+  uint32_t timeOutTimer;
   Wire.beginTransmission(device);
   Wire.write(address);
-  Wire.endTransmission();
+  uint8_t return_code = Wire.endTransmission(false); //False so it does not release bus
+  if (return_code) {
+    Serial.print(F("readFrom failed: "));
+    Serial.println(return_code);
+    return return_code; // See: http://arduino.cc/en/Reference/WireEndTransmission
+  }
 
-  Wire.beginTransmission(device);
-  Wire.requestFrom(device, num_bytes);
+  Wire.requestFrom(device, num_bytes, (uint8_t)true);
 
   int i = 0;
   while(Wire.available()) {
